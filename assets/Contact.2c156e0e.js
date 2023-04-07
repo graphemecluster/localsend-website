0813f7364f2d0de82ac203145ca33673e137e236
Tienisto <Tienisto@users.noreply.github.com>
2023-04-07 00:13:18 +0000

    deploy: 7e268bd46d5bf46fe71960bb6ce3e2e460fe0861

--- assets/Contact.2c156e0e.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./LegalLayout.vue_vue_type_script_setup_true_lang.26ad791c.js";import{d as a,l as e,w as o,o as s,f as c}from"./index.0db4932f.js";import"./mdi.a32159ae.js";const n=c("h1",{class:"text-h6"},"Contact: localsendapp@gmail.com",-1),f=a({__name:"Contact",setup(_){return(r,l)=>(s(),e(t,null,{default:o(()=>[n]),_:1}))}});export{f as default};
