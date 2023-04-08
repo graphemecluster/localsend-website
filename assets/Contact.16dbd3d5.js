422400d035db071f402a8070542ac03758352217
Tienisto <Tienisto@users.noreply.github.com>
2023-04-08 18:40:20 +0000

    deploy: b930214f5f70006529ad027150cbb8c90e5d32e1

---
+++ assets/Contact.16dbd3d5.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./LegalLayout.vue_vue_type_script_setup_true_lang.cc47e161.js";import{d as a,l as e,w as o,o as s,f as c}from"./index.254a8d9e.js";import"./mdi.a32159ae.js";const n=c("h1",{class:"text-h6"},"Contact: localsendapp@gmail.com",-1),f=a({__name:"Contact",setup(_){return(r,l)=>(s(),e(t,null,{default:o(()=>[n]),_:1}))}});export{f as default};
--- assets/Contact.e3120d72.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./LegalLayout.vue_vue_type_script_setup_true_lang.ecf16d55.js";import{d as a,l as e,w as o,o as s,f as c}from"./index.7d800614.js";import"./mdi.a32159ae.js";const n=c("h1",{class:"text-h6"},"Contact: localsendapp@gmail.com",-1),f=a({__name:"Contact",setup(_){return(r,l)=>(s(),e(t,null,{default:o(()=>[n]),_:1}))}});export{f as default};
