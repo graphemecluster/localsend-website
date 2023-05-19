bd19fb3003f5ecb2cda06dec204c7f9c82fd20ef
Tienisto <Tienisto@users.noreply.github.com>
2023-05-19 21:30:33 +0000

    deploy: bd07356159a92127a6c5d3b32db6f2c8e444f219

---
+++ assets/Contact.4a740d24.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./LegalLayout.vue_vue_type_script_setup_true_lang.018e3bec.js";import{d as a,N as e,w as o,o as s,f as c}from"./index.a39053c9.js";import"./mdi.be1be72f.js";const n=c("h1",{class:"text-h6"},"Contact: localsendapp@gmail.com",-1),f=a({__name:"Contact",setup(_){return(r,m)=>(s(),e(t,null,{default:o(()=>[n]),_:1}))}});export{f as default};
--- assets/Contact.fb6adfac.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./LegalLayout.vue_vue_type_script_setup_true_lang.fe6069da.js";import{d as a,N as e,w as o,o as s,f as c}from"./index.4a4d58c7.js";import"./mdi.be1be72f.js";const n=c("h1",{class:"text-h6"},"Contact: localsendapp@gmail.com",-1),f=a({__name:"Contact",setup(_){return(r,m)=>(s(),e(t,null,{default:o(()=>[n]),_:1}))}});export{f as default};
